0b77481708ee74ff94786ce03b965be92d42ca90
MohamedAmineOessleti <ouessletiimohamed@gmail.com>
2024-04-25 11:59:32 +0100

    Amine

--- client/App.js
+++ client/App.js
@@ -4,48 +4,21 @@
  *
  * @format
  */
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert, StyleSheet, FlatList ,Text} from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+ 
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
-
-
-
+  return (
+    <View >
+   <Text>hello </Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+
 });
 
 export default App;
