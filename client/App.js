2111537080c230bbb89f69d2f16df71495fdb136
MohamedAmineOessleti <ouessletiimohamed@gmail.com>
2024-04-26 13:23:39 +0100

    Amine

--- client/App.js
+++ client/App.js
@@ -1,25 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-import React, { useState, useEffect } from 'react';
+import * as React from 'react';
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, useColorScheme, View, ImageBackground, ActivityIndicator } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import   Home from './components/tes'
-
-
-function App() {
-  
-
+import MapCoffeShop from './components/AllCoffeShops'
+const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Home/>
-    </SafeAreaView>
-  );
-}
-
-
+  <MapCoffeShop/>
+  
+)};
 
 export default App;
