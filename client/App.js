93db7febe6c3ee7e9404f875cb3733835c857c22
ihebghdifi <ihabghdifi@gmail.com>
2024-04-26 18:27:07 +0100

    done

--- client/App.js
+++ client/App.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, useColorScheme, View, ImageBackground, ActivityIndicator } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import   Home from './components/tes'
+// import { SafeAreaView, StyleSheet, Text, TouchableOpacity, useColorScheme, View, ImageBackground, ActivityIndicator } from 'react-native';
+import { View, Text, TextInput,SafeAreaView, StyleSheet,ScrollView, Image } from 'react-native';
+import { IconButton ,Button} from 'react-native-paper';
+import Profile from './screens/Navigation';
 
 
 function App() {
-  
+   
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Home/>
+      {/* <Profile/>  */}
+          
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        
+   <Button icon="arrow-left"   size={20} textColor='black' onPress={() => navigation.goBack()}>
+</Button>
+  <Button icon="dots-vertical" size={20} textColor='black' onPress={() => navigation.goBack()}>
+  </Button>
+</View>
+      <Profile/>
     </SafeAreaView>
   );
 }
