77bc070821e37b01330864c06f24c793822f1105
AzizLajnef <zvenino7@gmail.com>
2024-04-27 00:02:25 +0100

    done

--- client/App.js
+++ client/App.js
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, useColorScheme, View, ImageBackground, ActivityIndicator } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import   Home from './components/tes'
-
-
+import ProductList from './components/ProductList'
 function App() {
   
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Home/>
+      <ProductList />
     </SafeAreaView>
   );
 }
