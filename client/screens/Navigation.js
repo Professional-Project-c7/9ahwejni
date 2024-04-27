7e485b9eeb2bdc89c9eba4a13c448bf3562177f5
MohamedAmineOessleti <ouessletiimohamed@gmail.com>
2024-04-27 17:54:44 +0100

    Amine

--- client/screens/Navigation.js
+++ client/screens/Navigation.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-// import * as React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import test from '../components/start.js'
-// import user from '../components/UserProfile.js'
-
-// const Tab = createBottomTabNavigator();
-
-// export default function App() {
-//   return (
-    
-//     <NavigationContainer>
-//       <Tab.Navigator>
-//         <Tab.Screen name="Home" component={test} options={{headerShown:false}} />
-//         <Tab.Screen name="cofe Shope" component={test} options={{headerShown:false}} />
-//         <Tab.Screen name="Notification" component={user} options={{headerShown:false}} />
-//         <Tab.Screen name="profile" component={user} options={{headerShown:false}}  />
-//         {/* <Tab.Screen name="user4" component={user} />
-//         <Tab.Screen name="user5" component={user} />
-//         <Tab.Screen name="user6" component={user} /> */}
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-=======
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -51,4 +25,3 @@ function NAV() {
 }
 
 export default NAV;
->>>>>>> 30f029b10d09f44c29a3233cfb5a50957d4e6f49
