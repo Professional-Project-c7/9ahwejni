f3ff5f56f03611402ca8f27b3a9cabfe46115925
ihebghdifi <ihabghdifi@gmail.com>
2024-04-29 09:50:48 +0100

    done

--- client/screens/Navigation.js
+++ client/screens/Navigation.js
@@ -6,19 +6,21 @@ import SignCofee from '../components/SignCofee';
 import Login from '../components/Login';
 import SignACC from '../components/Signacc';
 import Start from '../components/start';
+import UserProfile from '../components/UserProfile'
+
 const Stack = createNativeStackNavigator();
 
 function NAVSTART() {
   return (
 
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="Home" component={Start}  />
+       <Stack.Navigator>
+      <Stack.Screen name="Start" component={Start}   options={{ headerShown: false }} />
       <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="UserSignUp" component={SignUser} />
         <Stack.Screen name="CoffeeShopSignUp" component={SignCofee} />
         <Stack.Screen name="UserAccount" component={SignACC} />
-
+        <Stack.Screen name="UserProfile" component={SignACC} />
 
       </Stack.Navigator>
     </NavigationContainer>
