065a52e9da72d9845627f916d7475a62710d0951
sassi <rami9b3@live.fr>
2024-04-29 18:44:00 +0100

    y

--- client/App.js
+++ client/App.js
@@ -2,16 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import NAVSTART from './screens/Navigation';
 import Start2 from './components/Start2';
-// import ProductList from './components/ProductList'; 
 
 function App() {
    
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {/* <Start2/> */}
     <NAVSTART/>
-      {/* <ProductList/> */}
     </SafeAreaView>
   );
 }
