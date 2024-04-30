71fc35093b5599438ec06db070cb23dc6e132725
MohamedAmineOessleti <ouessletiimohamed@gmail.com>
2024-04-30 09:12:07 +0100

    Amine

--- client/App.js
+++ client/App.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import NAVSTART from './screens/Navigation';
 import Start2 from './components/Start2';
-
+import MapCoffe from './components/MapCoffe';
 function App() {
    
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-    <NAVSTART/>
+    {/* <NAVSTART/> */}
+<MapCoffe/>
     </SafeAreaView>
   );
 }
