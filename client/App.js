f2064a807ebf285fa78549f49d23ed593ade4da4
sassi <rami9b3@live.fr>
2024-04-30 14:05:43 +0100

    yyy

--- client/App.js
+++ client/App.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import NAVSTART from './screens/Navigation';
-// import MapCoffe from './components/MapCoffe';
-
-
-// import ProductList from './components/ProductList'; 
+import MapCoffe from './components/MapCoffe';
 
 function App() {
    
  
   return (
     <SafeAreaView style={{ flex: 1 }}>
-    
     <NAVSTART/>
    
     </SafeAreaView>
@@ -21,4 +17,33 @@ function App() {
 
 
 export default App;
+
+
+
+// import 'react-native-gesture-handler';
+// import * as React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import HomeScreen from './screens/Navigation';
+// import DetailsScreen from './pages/start';
+// // import { View } from 'react-native-reanimated/lib/typescript/Animated';
+// import { SafeAreaView, StyleSheet, Text, TouchableOpacity, useColorScheme, View, ImageBackground, ActivityIndicator } from 'react-native';
+// import Login from './pages/Login';
+// const Stack = createStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Home" component={DetailsScreen} />
+//         <Stack.Screen name="Details" component={DetailsScreen} />
+//         <Stack.Screen name="login" component={Login} />
+
+//       </Stack.Navigator>
+//     </NavigationContainer>
+   
+//   );
+// }
+
+// export default App;
 
