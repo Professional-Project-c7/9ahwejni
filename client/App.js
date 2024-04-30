63fd4812d1111d6e90d1b7f3c9e2ade5314e94d9
sassi <rami9b3@live.fr>
2024-04-30 14:22:42 +0100

    yyy

--- client/App.js
+++ client/App.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import NAVSTART from './screens/Navigation';
 import MapCoffe from './components/MapCoffe';
+import Chat from './components/chat';
 
 function App() {
    
  
   return (
     <SafeAreaView style={{ flex: 1 }}>
-    <NAVSTART/>
+      <Chat/>
+    {/* <NAVSTART/> */}
    
     </SafeAreaView>
   );
