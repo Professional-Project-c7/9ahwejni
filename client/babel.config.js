aa4a030cd0ac4f930c977f68a9968fa1e2bc95cc
sassi <rami9b3@live.fr>
2024-04-30 17:36:01 +0100

    y

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset',flow],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
       'module:react-native-dotenv',
