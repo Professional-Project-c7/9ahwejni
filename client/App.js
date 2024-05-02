d2ced7e6c6bafbcffde557cc1435eadd33258c6d
dhiahamouda <dhiahammouda777@gmail.com>
2024-05-02 10:24:04 +0100

    ok

--- client/App.js
+++ client/App.js
@@ -1,36 +1,23 @@
 import React, { useState, useEffect } from 'react';
-<<<<<<< HEAD
 import { SafeAreaView } from 'react-native';
 import NAVSTART from './screens/Navigation';
-// import MapCoffe from './components/MapCoffe';
 import HomePage from './components/homepage';
-import MenuItems from './components/menuitems';
-import ProductList from './components/ProductList';
-=======
-import { LogBox, SafeAreaView } from 'react-native';
-import NAVSTART from './screens/Navigation';
-import MapCoffe from './components/MapCoffe';
-import Chat from './components/chat';
 
->>>>>>> 66406eebe27a6192a0ec680c265df153de7f16cf
 function App() {
-  //  
+   
   console.log(process.env.ipAdress);
   return (
     <SafeAreaView style={{ flex: 1 }}>
-<<<<<<< HEAD
     
-    <NAVSTART/>
+    <HomePage/>
 
-=======
-      {/* <Chat/> */}
-    <NAVSTART/>
-   
->>>>>>> 66406eebe27a6192a0ec680c265df153de7f16cf
     </SafeAreaView>
-  ); 
+  );
 }
 
 
 
 export default App;
+
+
+
---
+++ client/config.js
@@ -0,0 +1,5 @@
+const ipAdress = "192.168.11.130";
+
+module.exports = {
+    ipAdress: ipAdress
+};
