8bb415649f6f87755b53f7c1bf9ab30894b6b6bc
ihebghdifi <ihabghdifi@gmail.com>
2024-05-02 10:42:38 +0100

    d

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -3,15 +3,9 @@ import { View, Text, TextInput, StyleSheet, ScrollView, Button, Image } from 're
 import axios from 'axios';
 import { launchImageLibrary } from 'react-native-image-picker';
 import { Cloudinary } from 'cloudinary-react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'
-
-
-// import {Cloudinary} from "@cloudinary/url-gen";
-          
-
-  
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { ipAdress } from '../config';
+
 const UserProfile = () => {
   const [imageUrl, setImageUrl] = useState(imageUrl);
   const [profile, setProfile] = useState({
@@ -20,7 +14,14 @@ const UserProfile = () => {
     LastName: '',
     Adresse: '',
   });
- 
+  const [showSetting, setShowSetting] = useState(false); 
+  const [settingText, setSettingText] = useState(''); 
+
+  const user = {
+    avatar: "https://www.bootdey.com/img/Content/avatar/avatar1.png",
+    coverPhoto: "https://www.bootdey.com/image/280x280/FF00FF/000000",
+    name: "John Smith"
+  };
  
   useEffect(() => {
     fetchUserProfile();
@@ -59,54 +60,65 @@ const UserProfile = () => {
       }
     });
   };
+  const handleSettingClick = () => {
+    setShowSetting(true); // Show the setting view
+    setSettingText('Setting'); // Set the setting text
+  };
 
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Text style={styles.heading}>Profile</Text>
-        <View style={styles.profileInfo}>
-          <Image source={{ uri: imageUrl }} style={styles.image} />
-          <Button
-    title="+"
-  onPress={handleImageUpload}
-  icon={<Icon name="plus" size={75} color="black" />} // Add the icon to the button
-  style={styles.button} // Assuming you have defined a style for the button
-/>
+        <View style={styles.container}>
+          <Image source={{ uri: user.coverPhoto }} style={styles.coverPhoto} />
+          <View style={styles.avatarContainer}>
+            <Image source={{ uri: user.avatar }} style={styles.avatar} />
+            <Text style={styles.Name}>{profile.FirstName}</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+          <Button title="setting" onPress={handleSettingClick} />
+            <Button title="camande" onPress={() => {}} />
+          </View>
         </View>
-        <Text style={styles.Name}>{profile.FirstName}</Text>
-        <View style={styles.profileInfo}>
-          <Text style={[styles.label, { marginTop: 100 }]}>FirstName:</Text>
-          <TextInput
-            style={styles.input}
-            value={profile.FirstName}
-            onChangeText={value => setProfile({ ...profile, FirstName: value })}
-            placeholder="Enter your name"
-          />
-          <Text style={styles.label}>LastName:</Text>
-          <TextInput
-            style={styles.input}
-            value={profile.LastName}
-            onChangeText={value => setProfile({ ...profile, LastName: value })}
-            placeholder="Enter your last name"
-          />
-          <Text style={styles.label}>Password:</Text>
-          <TextInput
-            style={styles.input}
-            value={profile.Password}
-            onChangeText={value => setProfile({ ...profile, Password: value })}
-            placeholder="Enter your password"
-            secureTextEntry={true}
-          />
-          <Text style={styles.label}>Adresse:</Text>
-          <TextInput
-            style={styles.input}
-            value={profile.Adresse}
-            onChangeText={value => setProfile({ ...profile, Adresse: value })}
-            placeholder="Enter your address"
-          />
-        </View>
-        
-        <Button title="Save Changes" onPress={handleSave} />
+
+        {showSetting ? ( 
+          <View style={styles.settingContainer}>
+            <Text style={styles.settingText}>{settingText}</Text> {/* Display the setting text */}
+            <Button title="Close" onPress={() => setShowSetting(false)} /> {/* Close button to hide the setting view */}
+          </View>
+        ) : (
+          <View style={styles.profileInfo}>
+            <Text style={[styles.label, { marginTop: 100 }]}>FirstName:</Text>
+            <TextInput
+              style={styles.input}
+              value={profile.FirstName}
+              onChangeText={value => setProfile({ ...profile, FirstName: value })}
+              placeholder="Enter your name"
+            />
+            <Text style={styles.label}>LastName:</Text>
+            <TextInput
+              style={styles.input}
+              value={profile.LastName}
+              onChangeText={value => setProfile({ ...profile, LastName: value })}
+              placeholder="Enter your last name"
+            />
+            <Text style={styles.label}>Password:</Text>
+            <TextInput
+              style={styles.input}
+              value={profile.Password}
+              onChangeText={value => setProfile({ ...profile, Password: value })}
+              placeholder="Enter your password"
+              secureTextEntry={true}
+            />
+            <Text style={styles.label}>Adresse:</Text>
+            <TextInput
+              style={styles.input}
+              value={profile.Adresse}
+              onChangeText={value => setProfile({ ...profile, Adresse: value })}
+              placeholder="Enter your address"
+            />
+            <Button title="Save Changes" onPress={handleSave} />
+          </View>
+        )}
       </View>
     </ScrollView>
   );
@@ -131,16 +143,9 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     position: 'relative',
   },
-  image: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    marginTop: 30,
-    position: 'relative',
-    zIndex: 1,
-    borderWidth: 5,
-    borderColor: '#121212',
-    backgroundColor: 'white',
+  settingContainer: {
+    alignItems: 'center',
+    marginTop: 20,
   },
   label: {
     fontWeight: 'bold',
@@ -169,6 +174,38 @@ const styles = StyleSheet.create({
    backgroundColor:'black',
    color: 'black',
    marginLeft:900
+  },
+  coverPhoto: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+  },
+  avatarContainer: {
+    alignItems: 'center',
+    marginTop: -75,
+  },
+  avatar: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    borderWidth: 5,
+    borderColor: 'white',
+  },
+  name: {
+    marginTop: 15,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    width: '60%',
+    justifyContent: 'space-between',
+  },
+  settingText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
 
