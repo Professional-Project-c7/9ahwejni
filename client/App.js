4389fe538472ea095cb8c1a8f642d27b26181115
sassi <rami9b3@live.fr>
2024-05-04 13:33:12 +0100

    y

--- client/App.js
+++ client/App.js
@@ -1,49 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import NAVSTART from './screens/Navigation';
-// import MapCoffe from './components/MapCoffe';
-import HomePage from './components/homepage';
-import MenuItems from './components/menuitems';
+import MapCoffe from './components/MapCoffe';
 import ProductList from './components/ProductList';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import SellerPage from './components/SellerPage';
+import Paye from './components/Payment';
+
 function App() {
-  console.log(process.env.ipAdress);
   
+  console.log(process.env.ipAdress);
  return (
   <Provider store={store}>
     <SafeAreaView style={{ flex: 1 }}>
-    <ProductList />
+    <NAVSTART />
  </SafeAreaView>
  </Provider>
   );
 }
 export default App;
-
-
-
-// import 'react-native-gesture-handler';
-// import * as React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import HomeScreen from './screens/Navigation';
-// import DetailsScreen from './pages/start';
-// // import { View } from 'react-native-reanimated/lib/typescript/Animated';
-// import { SafeAreaView, StyleSheet, Text, TouchableOpacity, useColorScheme, View, ImageBackground, ActivityIndicator } from 'react-native';
-// import Login from './pages/Login';
-// const Stack = createStackNavigator();
-
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen name="Home" component={DetailsScreen} />
-//         <Stack.Screen name="Details" component={DetailsScreen} />
-//         <Stack.Screen name="login" component={Login} />
-
-//       </Stack.Navigator>
-//     </NavigationContainer>
-   
-//   );
-// }
-
-// export default App;
-
