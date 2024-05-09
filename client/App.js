56c275ba952e076ecf70f9c45befef3fdae1a4f7
MohamedAmineOessleti <ouessletiimohamed@gmail.com>
2024-05-09 10:35:44 +0100

    amine

--- client/App.js
+++ client/App.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import NAVSTART from './screens/Navigation';
-import ProductPacksList from './components/ProductPacksList';
+import Start from './components/Start2';
 import ProductList from './components/ProductList';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import AllCoffeShops from './components/UserProfile';
+import CoffeeShopsList from './components/AllCoffeShops';
 import HomePage from './components/homepage';
 import SellerPage from './components/SellerPage';
 import Paye from './components/Payment';
 import PaymentCardsDetails from './components/paymentcardsdetailsCoffee';
 import ProductDetailsPage from './components/ProdDetails';
-
+import Onboarding from './components/Onboarding';
 
 function App() {
 
@@ -19,7 +19,7 @@ function App() {
  return (
   <Provider store={store}>
     <SafeAreaView style={{ flex: 1 }}>
-    <ProductPacksList />
+    <Onboarding />
  </SafeAreaView>
  </Provider>
   );
