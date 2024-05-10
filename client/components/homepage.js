249f304785019e05d0e8d5cb4719c705e4b8b54a
sassi <rami9b3@live.fr>
2024-05-10 10:13:45 +0100

    kj

--- client/components/homepage.js
+++ client/components/homepage.js
@@ -4,9 +4,8 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, FlatList, StatusBar } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { IconButton } from 'react-native-paper';
-// import TrackPlayer, { useTrackPlayerProgress } from 'react-native-track-player';
-// import testMusic from '../music/testMusic.mp3';
-import MusicPlayer from './MusicPlayer'; // Import MusicPlayer component
+
+import MusicPlayer from './MusicPlayer'; 
 import RandomProducts from "../components/randomproducts";
 import Pub from "../components/pub";
 import TopShops from "../components/TopShops";
