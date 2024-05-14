bd35a2bf09058b12657c2196b7fa16815148130f
AzizLajnef <zvenino7@gmail.com>
2024-05-14 11:56:16 +0100

    done

--- client/components/randomproducts.js
+++ client/components/randomproducts.js
@@ -1,7 +1,3 @@
-
-
-
-
 import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
@@ -15,36 +11,48 @@ import {
 } from 'react-native';
 import Sound from 'react-native-sound';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
 import { useNavigation } from '@react-navigation/native';
-
 import { Rating } from 'react-native-ratings';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import axios from 'axios';
 import { ipAdress } from '../config';
 
 const RandomProducts = () => {
-  const navigation = useNavigation(); 
-
+  const navigation = useNavigation();
   const [products, setProducts] = useState([]);
   const [favorites, setFavorites] = useState({});
   const [hearttt, sethearttt] = useState({});
-  
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get(`http://${ipAdress}:3000/api/product/`);
-        const shuffledProducts = response.data.sort(() => 0.5 - Math.random());
-        setProducts(shuffledProducts.slice(0,4 ));
+        const productsResponse = await axios.get(`http://${ipAdress}:3000/api/product`);
+        const reviewsResponse = await axios.get(`http://${ipAdress}:3000/api/review`);
+
+        // Mapping products to include review data
+        const productsWithReviews = productsResponse.data.map(product => {
+          const productReviews = reviewsResponse.data.filter(review => review.prodId === product.id);
+          const totalReviews = productReviews.length;
+          const averageRating = totalReviews ? productReviews.reduce((acc, review) => acc + review.stars, 0) / totalReviews : 0;
+          return {
+            ...product,
+            totalReviews,
+            averageRating: averageRating.toFixed(1),
+          };
+        });
+
+        // Sorting products by averageRating in descending order and taking top 4
+        const topRatedProducts = productsWithReviews.sort((a, b) => b.averageRating - a.averageRating).slice(0, 4);
+
+        setProducts(topRatedProducts);
       } catch (err) {
         setError(err.message);
       }
     };
     fetchProducts();
-    // AsyncStorage.clear()
   }, []);
+
   const playAlertSound = () => {
     const alertSound = new Sound('alert.mp3', Sound.MAIN_BUNDLE, (error) => {
       if (error) {
@@ -61,6 +69,7 @@ const RandomProducts = () => {
       });
     });
   };
+
   const toggleFeature = async (id, feature) => {
     try {
       const isFavorited = favorites[id]?.[feature];
@@ -75,36 +84,24 @@ const RandomProducts = () => {
             [feature]: true,
           },
         }));
-  
         favoritesArray.push(product);
         await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray));
-          // Alert.alert('he add in panier');
-  
         Alert.alert('Item added to cart');
         playAlertSound();
-  
-        // setTimeout(() => {
-        //   Alert.alert('');
-        // }, 2000);
       }
     } catch (error) {
       console.log('Error toggling feature:', error);
     }
   };
-  
-
-
 
   const handleNavigateToDetails = async (product) => {
     try {
-     
       await AsyncStorage.setItem('selectedProductId', product.id.toString());
       navigation.navigate('prd', { product });
     } catch (error) {
       console.log('Error storing selected product ID:', error);
     }
   };
-
 
   return (
     <SafeAreaView style={styles.container}>
@@ -122,26 +119,22 @@ const RandomProducts = () => {
                   name={hearttt[product.id]?.favored ? 'heart' : 'heart-outline'}
                   color={hearttt[product.id]?.favored ? 'red' : '#dba617'}
                   size={27}
-                  // onPress={() => toggleFeature(product.id, 'hearttt')}
                   style={styles.favIcon}
                 />
                 <View style={styles.infoContainer}>
-                  <TouchableOpacity 
-                
-                  >
-                    <Text style={styles.name}>{product.name}</Text>
-                  </TouchableOpacity>
+                  <Text style={styles.name}>{product.name}</Text>
                   <Text style={styles.price}>${product.price}</Text>
+                  <Text style={styles.reviews}>{`${product.totalReviews} 👤 ⭐: ${product.averageRating}`}</Text>
                   <Rating
                     type="star"
                     ratingCount={5}
                     imageSize={20}
-                    startingValue={product.rating}
+                    startingValue={parseFloat(product.averageRating)}
                     onFinishRating={(rating) => console.log('New rating is: ', rating)}
                     style={styles.starRating}
                   />
                   <Icon
-                    name={favorites[product.id]?.inCart ? 'cart' : 'cart'}
+                    name={favorites[product.id]?.inCart ? 'cart' : 'cart-outline'}
                     size={24}
                     onPress={() => toggleFeature(product.id, 'inCart')}
                     style={styles.cartIcon}
@@ -204,8 +197,13 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   price: {
-    fontSize: 20,
+    fontSize: 21,
     color: '#000',
+    marginTop: 5,
+  },
+  reviews: {
+    fontSize: 16,
+    color: '#646464',
     marginTop: 5,
   },
   starRating: {
@@ -222,10 +220,3 @@ const styles = StyleSheet.create({
 });
 
 export default RandomProducts;
-
-
-
-
-
-
-
