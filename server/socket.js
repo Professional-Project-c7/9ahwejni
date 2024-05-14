c117a71f79bb7310fcda241c5ce063fc69d29162
MohamedAmineOessleti <ouessletiimohamed@gmail.com>
2024-05-14 13:07:46 +0100

    amine

--- server/socket.js
+++ server/socket.js
@@ -12,63 +12,53 @@ const io = socketIo(server, {
   },
 });
 
-// Store user ID and socket mapping
 const userSockets = {};
-const connectedUsers = {}; // To keep track of connected users
+const connectedUsers = {};
 
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`);
 
-  // Receive user ID from the client
   const userId = socket.handshake.query.userId;
   if (userId) {
-    console.log(`User ID: ${userId}`); // Log the user ID
+    console.log(`User ID: ${userId}`);
     userSockets[userId] = socket.id;
-    connectedUsers[userId] = true; // Mark user as connected
+    connectedUsers[userId] = true;
 
-    // Join the global room
     socket.join('global');
   }
 
   socket.on('send_message', (data, callback) => {
-    // Extract message data
     const { recipientId, content, timestamp } = data;
-  
-    // Create the complete message object
     const message = {
       senderId: userId,
       content,
-      timestamp: timestamp || new Date().toLocaleString(), // Use provided timestamp or current time
+      timestamp: timestamp || new Date().toLocaleString(),
     };
-  
-    const recipientSocketId = userSockets[recipientId];
-    if (recipientSocketId) {
-      io.to(recipientSocketId).emit('receive_message', message);
+
+    if (recipientId && userSockets[recipientId]) {
+      io.to(userSockets[recipientId]).emit('receive_message', message);
     } else {
-      // Send message to all users in the global room
       io.to('global').emit('receive_message', message);
-      callback('success');
     }
+
+    callback('success');
   });
-  
 
   socket.on('disconnect', () => {
     console.log(`User disconnected: ${socket.id}`);
-    // Remove user ID and socket mapping upon disconnection
     for (const [key, value] of Object.entries(userSockets)) {
       if (value === socket.id) {
         delete userSockets[key];
-        delete connectedUsers[key]; // Remove user from connected users list
+        delete connectedUsers[key];
         break;
       }
     }
   });
 });
 
-// Endpoint to check if a user is connected
 app.get('/isConnected', (req, res) => {
-  const userId = req.headers.userid; // Retrieve user ID from headers
-  const isConnected = connectedUsers[userId]? true : false;
+  const userId = req.headers.userid;
+  const isConnected = !!connectedUsers[userId];
   res.json({ isConnected });
 });
 
