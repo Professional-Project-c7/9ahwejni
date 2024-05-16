a3b69840c79b96955c30caba56de1f4e50e55177
sassi <rami9b3@live.fr>
2024-05-16 19:02:32 +0100

    y

--- client/components/allpack.js
+++ client/components/allpack.js
@@ -188,7 +188,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     overflow: 'hidden',
     shadowColor: '#000',
-    
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.1,
     shadowRadius: 6,
