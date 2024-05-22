eff4303856ffd2135d0b7a3af9ccd534daeaf631
ihebghdifi <ihabghdifi@gmail.com>
2024-05-22 17:21:45 +0100

    f

--- client/screens/Navigation.js
+++ client/screens/Navigation.js
@@ -183,7 +183,7 @@ function TabNavigator() {
         <Tab.Screen name="Edit" component={EditCoffee}  options={{
           tabBarButton: () => null, // This hides the tab from the tab bar
         }} />
-         <Tab.Screen name="editusers" component={editusers}  options={{
+         <Tab.Screen name="editusers" component={editusers}   s={{
           tabBarButton: () => null, // This hides the tab from the tab bar
         }} />
          <Tab.Screen name="TransactionScreenCoffee" component={TransactionScreenCoffee}  options={{
